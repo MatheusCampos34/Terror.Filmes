71119e3db74f3e99c81022f926bf793dcefee21e
matheuscamposbr34 <matheuscamposbr34@gmail.com>
2021-09-20 02:02:22 -0300

    Adicionar arquivos de projeto.

---
+++ Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Terror_Alemão = 1,
+		Terror_Frances = 2,
+		Terror_Psicologico = 3,
+		Terror_com_Bruxas = 4,
+		Terror_Leve = 5,
+		Terror_Gore = 6,
+		Terror_Ocidental  = 7,
+		Terror_Oriental = 8,
+		Terror_Futuristico = 9,
+		Terror_com_suspense = 10,
+		Terror_Nacional = 11,
+		Terror_Espiritual = 12,
+		Terror_Aleatorio = 13
+	}
+}
